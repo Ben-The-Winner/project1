844086aad4ef78a6057a362577c38124ed9efd6e
Ben-The-Winner <ben6119070@gmail.com>
2022-04-30 01:02:15 -0700

    refactored step 1

--- damusic-master/main.py
+++ damusic-master/main.py
@@ -2,6 +2,7 @@
 
 import librosa
 from tkinter import *
+from pandas import array
 import pymongo
 from playsound import playsound
 from songify import songify
@@ -24,19 +25,12 @@ input.insert(0,"enter file or press an option button: ")
 def play():
     playsound(input.get(), block= False)
     
-def play1():
-    playsound("sample1 (copy).wav", block=False)
-
-def play2():
-    playsound("sample3.wav",block=False)
-
-def play3():
-    playsound("stereo_file.wav",block=False)
+def play3_wav():
+    playsound("the_song_files/sample3.wav",block=False)
     
 def stop():
     stop()       
     
-
 
  
 # making a button which trigger the function so sound can be played
@@ -48,21 +42,42 @@ stop_button = Button(root, text="Stop", font=("Helvetica", 15),
 relief=GROOVE, command=stop,bg="green")
 stop_button.pack(pady=20)
 
-play_button1 = Button(root, text="Play Option1: sample1 (copy).wav", font=("Helvetica", 10),
-relief=GROOVE, command=play1,bg="green")
-play_button1.pack(pady=20)
 
-play_button2 = Button(root, text="Play Option2: sample3.wav", font=("Helvetica", 10),
-relief=GROOVE, command=play2,bg="green")
-play_button2.pack(pady=20)
+play_sample3_wav = Button(root, text="Play sample3.wav", font=("Helvetica", 10),
+relief=GROOVE, command=play3_wav,bg="green")
+play_sample3_wav.pack(pady=20)
 
-play_button3 = Button(root, text="Play Option3: stereo_file.wav", font=("Helvetica", 10),
-relief=GROOVE, command=play3,bg="green")
-play_button3.pack(pady=20)
 
 def convert(lst):
     res_dct = {str(i): lst[i] for i in range(0, len(lst), 1)}
     return res_dct
+
+def load_file(filename):
+    client = pymongo.MongoClient("mongodb+srv://ben6119070:BL246810@cluster0.qojmx.mongodb.net/OnABetterNote?retryWrites=true&w=majority")
+    dbcol= client["OnABetterNote"]["Notes"]
+    result = {}
+    # 2. Load the audio as a waveform `y`, Store the sampling rate as `sr`
+    y, sr = librosa.load(filename)
+
+    # 3. Extract the notes 
+    notes = librosa.midi_to_note(range(0,12), key='B:min')
+    result["original_notes"] = notes
+    sf.write('stereo_file.wav', y, 44100, 'PCM_24')
+    res=convert(notes)
+    dbcol.insert_one(res)
+    # 4. Rearrange them with an algo from songify.py
+    s_notes = songify(dbcol)
+    result["shuffled_notes"] = s_notes
+
+    # 5. Run the default beat tracker
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    result["tempo"] = tempo
+
+    # 6. Convert the frame indices of beat events into timestamps
+    result["beat_times"] = librosa.frames_to_time(beat_frames, sr=sr)
+    return result
+
+
 
 def custom_play_notes(notes_list):
     #recieving  list of notes and play them with custom sound
@@ -75,45 +90,25 @@ def custom_play_notes(notes_list):
             
     return
 
-client = pymongo.MongoClient("mongodb+srv://ben6119070:BL246810@cluster0.qojmx.mongodb.net/OnABetterNote?retryWrites=true&w=majority")
-dbcol= client["OnABetterNote"]["Notes"]
 
 # 1. Get the file path to an included audio example
-filename = librosa.example('nutcracker')
-ipd.Audio(filename)
-# 2. Load the audio as a waveform `y`, Store the sampling rate as `sr`
-y, sr = librosa.load(filename)
+filename = "stereo_file.wav"
+result = load_file(filename)
 
-# 3. Extract the notes 
-notes = librosa.midi_to_note(range(0,12), key='B:min')
-words = notes
-sf.write('stereo_file.wav', y, 44100, 'PCM_24')
-res=convert(notes)
-dbcol.insert_one(res)
-# 4. Rearrange them with an algo from songify.py
-notes = songify(dbcol)
-words2 = notes
-
-# 5. Run the default beat tracker
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-words3 = tempo
-
-# 6. Convert the frame indices of beat events into timestamps
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
 
 
 def click_display_original_notes():
-    myLabel = Label(root, text = words)
+    myLabel = Label(root, text = result["original_notes"])
     myLabel.pack()
 
 def click_play_shuffled_notes():
-    myLabel2 = Label(root, text = words2)
+    myLabel2 = Label(root, text = result["shuffled_notes"])
     myLabel2.pack()
-    custom_play_notes(words2)
+    custom_play_notes(result["shuffled_notes"])
 
 def click_display_tempo():
-    myLabel3 = Label(root, text = words3)
+    myLabel3 = Label(root, text = result["tempo"])
     myLabel3.pack()
 
 
